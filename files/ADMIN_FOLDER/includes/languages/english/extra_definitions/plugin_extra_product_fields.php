2a9d99e2c0316cae3da9b2655819c8591e8f307a
Steve <4391026+torvista@users.noreply.github.com>
2021-11-07 21:56:52 +0100

    add links to Google taxonomy

--- files/ADMIN_FOLDER/includes/languages/english/extra_definitions/plugin_extra_product_fields.php
+++ files/ADMIN_FOLDER/includes/languages/english/extra_definitions/plugin_extra_product_fields.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 //Plugin Extra Product Fields
 //https://github.com/torvista/Zen_Cart-Extra_Product_Fields
 
@@ -6,7 +7,7 @@
 define('PLUGIN_EXTRA_PRODUCT_FIELDS_LABEL_EAN', 'EAN');
 define('PLUGIN_EXTRA_PRODUCT_FIELDS_PLACEHOLDER_EAN', 'EAN (13 numbers)');
 //Google Product Category
-define('PLUGIN_EXTRA_PRODUCT_FIELDS_LABEL_GOOGLE_PRODUCT_CATEGORY', 'Google Product Category');
+define('PLUGIN_EXTRA_PRODUCT_FIELDS_LABEL_GOOGLE_PRODUCT_CATEGORY', 'Google Product Category (<a href="https://www.google.com/basepages/producttype/taxonomy-with-ids.en-US.xls" target="_blank" title="Google taxonomy en-US Excel">Excel</a> | <a href="https://www.google.com/basepages/producttype/taxonomy.en-US.txt" target="_blank" title="Google taxonomy en-US TXT">TXT</a>)');
 define('PLUGIN_EXTRA_PRODUCT_FIELDS_PLACEHOLDER_GOOGLE_PRODUCT_CATEGORY', '6 numbers');
 //MPN
 define('PLUGIN_EXTRA_PRODUCT_FIELDS_LABEL_MPN', 'MPN');
